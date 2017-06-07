e009e92301f6094745b7afdf7812afa791b80149
James Mishra <james@anton.ai>
2017-06-07 15:06:58 -0700

    Broke out CSS and JS into separate files

---
+++ jekyll/content/js/algolia.js
@@ -0,0 +1,40 @@
+var results_entry =
+  '<h1 class="f3 f2-m f1-l"><a href="{{ url }}">{{{ _highlightResult.title.value }}}</a></h2>' +
+  '<p>{{{ _highlightResult.text.value }}}[...]</p>';
+
+var searchFunction = function(helper) {
+    container = document.getElementById('hitscontainercontainer');
+    if (helper.state.query == '') {
+        container.style.display = 'none';
+        return;
+    }
+    helper.search();
+    container.style.display = 'block';
+};
+
+var search = instantsearch({
+    appId: 'O2TYYMG19M',
+    apiKey: '0e17de69fee56cc29568d9fbb0003b62',
+    indexName: 'mlg',
+    searchFunction: searchFunction,
+    urlSync: true
+});
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#searchcontainer',
+    placeholder: 'Search for terms',
+    autofocus: false,
+    poweredBy: true
+  })
+);
+search.addWidget(
+  instantsearch.widgets.infiniteHits({
+    container: '#hitscontainer',
+    templates: {
+      empty: '<strong>No results</strong>',
+      item: results_entry
+    },
+    hitsPerPage: 3
+  })
+);
+search.start();
