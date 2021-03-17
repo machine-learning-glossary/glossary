fed27df9b21c2ede31ec92095281173ac51b2eee
James Mishra <james.mishra@neocrym.com>
2021-03-17 05:22:25 -0500

    fix build issues

--- serve.sh
+++ serve.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # This runs "jekyll serve" inside the container, which will build the website.
-docker run -v $PWD:/jekyll -p 4000:4000 --rm jekyll serve "$@"
+exec docker run \
+    --rm \
+    -it \
+    --init \
+    --volume="${PWD}":/opt/buildhome/repo \
+    --workdir=/opt/buildhome/repo \
+    --entrypoint=/opt/build-bin/build \
+    -p 4000:4000 \
+    netlify/build:xenial \
+    jekyll serve --host=0.0.0.0 --incremental
